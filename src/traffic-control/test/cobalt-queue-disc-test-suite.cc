d7365bf360a5ec9dadcf17a7bb6a0c3ecfd7de2a
shefaligups11 <Shefaligups11@gmail.com>
2019-03-22 17:54:03 +0530

    Update TestSuite for COBALT

--- src/traffic-control/test/cobalt-queue-disc-test-suite.cc
+++ src/traffic-control/test/cobalt-queue-disc-test-suite.cc
@@ -8,31 +8,6 @@
 #include "ns3/simulator.h"
 
 using namespace ns3;
-
-// The following code borrowed from Linux cobalt.h, for unit testing
-#define REC_INV_SQRT_BITS_ns3 (8 * sizeof(uint16_t))
-/* or sizeof_in_bits(rec_inv_sqrt) */
-/* needed shift to get a Q0.32 number from rec_inv_sqrt */
-#define REC_INV_SQRT_SHIFT_ns3 (32 - REC_INV_SQRT_BITS_ns3)
-
-static uint16_t _codel_Newton_step (uint32_t count, uint16_t rec_inv_sqrt)
-{
-  uint32_t invsqrt = ((uint32_t)rec_inv_sqrt) << REC_INV_SQRT_SHIFT_ns3;
-  uint32_t invsqrt2 = ((uint64_t)invsqrt * invsqrt) >> 32;
-  uint64_t val = (3LL << 32) - ((uint64_t)count * invsqrt2);
-
-  val >>= 2;
-  val = (val * invsqrt) >> (32 - 2 + 1);
-  return (val >> REC_INV_SQRT_SHIFT_ns3);
-}
-
-static uint32_t _reciprocal_scale (uint32_t val, uint32_t ep_ro)
-{
-  return (uint32_t)(((uint64_t)val * ep_ro) >> 32);
-}
-
-// End Linux borrow
-
 /**
  * \ingroup traffic-control-test
  * \ingroup tests
@@ -73,7 +48,7 @@ private:
 
 CobaltQueueDiscTestItem::CobaltQueueDiscTestItem (Ptr<Packet> p, const Address & addr,uint16_t protocol, bool ecnCapable)
   : QueueDiscItem (p, addr, ecnCapable),
-  m_ecnCapablePacket (ecnCapable)
+    m_ecnCapablePacket (ecnCapable)
 {
 }
 
@@ -226,264 +201,7 @@ CobaltQueueDiscBasicEnqueueDequeue::DoRun (void)
  * \ingroup traffic-control-test
  * \ingroup tests
  *
- * \brief Test 2: NewtonStep unit test - test against explicit port of Linux implementation
- */
-class CobaltQueueDiscNewtonStepTest : public TestCase
-{
-public:
-  CobaltQueueDiscNewtonStepTest ();
-  virtual void DoRun (void);
-};
-
-CobaltQueueDiscNewtonStepTest::CobaltQueueDiscNewtonStepTest ()
-  : TestCase ("NewtonStep arithmetic unit test")
-{
-}
-
-void
-CobaltQueueDiscNewtonStepTest::DoRun (void)
-{
-  Ptr<CobaltQueueDisc> queue = CreateObject<CobaltQueueDisc> ();
-
-  // Spot check a few points in the expected operational range of
-  // CobaltQueueDisc's m_count and m_recInvSqrt variables
-  uint32_t count = 2;
-  uint16_t recInvSqrt = 65535;
-  queue->m_count = count;
-  queue->m_recInvSqrt = recInvSqrt;
-  queue->NewtonStep ();
-  // Test that ns-3 value is exactly the same as the Linux value
-  NS_TEST_ASSERT_MSG_EQ (_codel_Newton_step (count, recInvSqrt), queue->m_recInvSqrt,
-                         "ns-3 NewtonStep() fails to match Linux equivalent");
-
-  count = 4;
-  recInvSqrt = 36864;
-  queue->m_count = count;
-  queue->m_recInvSqrt = recInvSqrt;
-  queue->NewtonStep ();
-  // Test that ns-3 value is exactly the same as the Linux value
-  NS_TEST_ASSERT_MSG_EQ (_codel_Newton_step (count, recInvSqrt), queue->m_recInvSqrt,
-                         "ns-3 NewtonStep() fails to match Linux equivalent");
-}
-
-/**
- * \ingroup traffic-control-test
- * \ingroup tests
- *
- * \brief Test 3: ControlLaw unit test - test against explicit port of Linux implementation
- */
-class CobaltQueueDiscControlLawTest : public TestCase
-{
-public:
-  CobaltQueueDiscControlLawTest ();
-  virtual void DoRun (void);
-  /**
-   * Codel control law function
-   * \param queue the queue disc
-   * \param t
-   * \returns the codel control law
-   */
-  uint32_t _cobalt_control_law (Ptr<CobaltQueueDisc> queue, uint32_t t);
-};
-
-CobaltQueueDiscControlLawTest::CobaltQueueDiscControlLawTest ()
-  : TestCase ("ControlLaw arithmetic unit test")
-{
-}
-
-// The following code borrowed from Linux codel.h,
-// except the addition of queue parameter
-uint32_t
-CobaltQueueDiscControlLawTest::_cobalt_control_law (Ptr<CobaltQueueDisc> queue, uint32_t t)
-{
-  return t + _reciprocal_scale (queue->Time2CoDel (queue->m_interval), queue->m_recInvSqrt << REC_INV_SQRT_SHIFT_ns3);
-}
-// End Linux borrrow
-
-void
-CobaltQueueDiscControlLawTest::DoRun (void)
-{
-  Ptr<CobaltQueueDisc> queue = CreateObject<CobaltQueueDisc> ();
-
-  /* Spot check a few points of m_dropNext
-   The integer approximations in Linux should be within
-   2% of the true floating point value obtained in ns-3
-   */
-  uint32_t dropNextTestVals [4] = {292299, 341128, 9804717, 55885007};
-
-  for (int i = 0; i < 4; ++i)
-    {
-      uint32_t ns3Result = queue->ControlLaw (dropNextTestVals[i]);
-      uint32_t upperBound = ns3Result + 0.02 * ns3Result;
-      uint32_t lowerBound = ns3Result - 0.02 * ns3Result;
-      uint32_t linuxResult = _cobalt_control_law (queue, dropNextTestVals[i]);
-      NS_TEST_EXPECT_MSG_EQ ((lowerBound < linuxResult || linuxResult < upperBound), true,
-                             "Linux result should stay within 2% of ns-3 result");
-    }
-}
-
-/**
- * \ingroup traffic-control-test
- * \ingroup tests
- *
- * \brief Test 4,5,6: ECN tests
- */
-class CobaltQueueDiscECNtest : public TestCase
-{
-public:
-  CobaltQueueDiscECNtest ();
-  virtual void DoRun (void);
-  /**
-   * Enqueue function
-   * \param queue the queue disc
-   * \param size the size
-   * \param nPkt the number of packets
-   * \param ecnCapable ECN capable flag
-   */
-  void Enqueue (Ptr<CobaltQueueDisc> queue, uint32_t size, uint32_t nPkt, bool ecnCapable);
-  /**
-   * Run Cobalt test function
-   * \param mode the mode
-   */
-
-  void RunECNtest (QueueSizeUnit mode);
-  void Dequeue (Ptr<CobaltQueueDisc> queue, uint32_t nPkt);
-  void DequeueWithDelay (Ptr<CobaltQueueDisc> queue, double delay, uint32_t nPkt);
-};
-
-CobaltQueueDiscECNtest::CobaltQueueDiscECNtest ()
-  : TestCase ("ECN verification tests, for both packets and bytes mode")
-{
-}
-
-void
-CobaltQueueDiscECNtest::RunECNtest (QueueSizeUnit mode)
-{
-  uint32_t pktSize = 1500;
-  uint32_t modeSize = 0;
-  Ptr<CobaltQueueDisc> queue = CreateObject<CobaltQueueDisc> ();
-  CobaltQueueDisc::Stats st;
-
-  // Test 4: Cobalt queue disc is ECN enabled, but packets are not ECN capable
-  queue = CreateObject<CobaltQueueDisc> ();
-
-  if (mode == QueueSizeUnit::BYTES)
-    {
-      modeSize = pktSize;
-    }
-  else if (mode == QueueSizeUnit::PACKETS)
-    {
-      modeSize = 1;
-    }
-
-  NS_TEST_EXPECT_MSG_EQ (queue->SetAttributeFailSafe ("MaxSize", QueueSizeValue (QueueSize (mode,modeSize * 1500))),
-                         true, "Verify that we can actually set the attribute MaxSize");
-  NS_TEST_EXPECT_MSG_EQ (queue->SetAttributeFailSafe ("UseEcn", BooleanValue (true)), true,
-                         "Verify that we can actually set the attribute UseECN");
-
-  queue->Initialize ();
-  if (mode == QueueSizeUnit::BYTES)
-    {
-      Enqueue (queue, pktSize, 100, false);
-    }
-  else
-    {
-      Enqueue (queue, 1, 100, false);
-    }
-  DequeueWithDelay (queue, 0.005, 100);
-  Simulator::Stop (Seconds (8.0));
-  Simulator::Run ();
-  st = StaticCast<CobaltQueueDisc> (queue)->GetStats ();
-  NS_TEST_EXPECT_MSG_NE (st.forcedDrop, 0, "There should be some forced drops");
-  NS_TEST_EXPECT_MSG_EQ (st.forcedMark, 0, "There should no forced marks");
-
-  // Test 5: Packets are ECN capable, but Cobalt queue disc is not ECN enabled
-  queue = CreateObject<CobaltQueueDisc> ();
-  NS_TEST_EXPECT_MSG_EQ (queue->SetAttributeFailSafe ("MaxSize", QueueSizeValue (QueueSize (mode,modeSize * 1500))),
-                         true, "Verify that we can actually set the attribute MaxSize");
-  NS_TEST_EXPECT_MSG_EQ (queue->SetAttributeFailSafe ("UseEcn", BooleanValue (false)), true,
-                         "Verify that we can actually set the attribute UseECN");
-
-  queue->Initialize ();
-  if (mode == QueueSizeUnit::BYTES)
-    {
-      Enqueue (queue, pktSize, 100, true);
-    }
-  else
-    {
-      Enqueue (queue, 1, 100, true);
-    }
-  DequeueWithDelay (queue, 0.005, 100);
-  Simulator::Stop (Seconds (8.0));
-  Simulator::Run ();
-  st = StaticCast<CobaltQueueDisc> (queue)->GetStats ();
-  NS_TEST_EXPECT_MSG_NE (st.forcedDrop, 0, "There should be some forced drops");
-  NS_TEST_EXPECT_MSG_EQ (st.forcedMark, 0, "There should no forced marks");
-
-  // Test 6: Packets are ECN capable and Cobalt queue disc is ECN enabled
-  queue = CreateObject<CobaltQueueDisc> ();
-  NS_TEST_EXPECT_MSG_EQ (queue->SetAttributeFailSafe ("MaxSize", QueueSizeValue (QueueSize (mode,modeSize * 1500))),
-                         true, "Verify that we can actually set the attribute MaxSize");
-  NS_TEST_EXPECT_MSG_EQ (queue->SetAttributeFailSafe ("UseEcn", BooleanValue (true)), true,
-                         "Verify that we can actually set the attribute UseECN");
-
-  queue->Initialize ();
-  if (mode == QueueSizeUnit::BYTES)
-    {
-      Enqueue (queue, pktSize, 100, true);
-    }
-  else
-    {
-      Enqueue (queue, 1, 100, true);
-    }
-  DequeueWithDelay (queue, 0.005, 100);
-  Simulator::Stop (Seconds (8.0));
-  Simulator::Run ();
-  st = StaticCast<CobaltQueueDisc> (queue)->GetStats ();
-  NS_TEST_EXPECT_MSG_NE (st.forcedMark, 0, "There should be some forced marks");
-}
-
-void
-CobaltQueueDiscECNtest::Enqueue (Ptr<CobaltQueueDisc> queue, uint32_t size, uint32_t nPkt, bool ecnCapable)
-{
-  Address dest;
-  for (uint32_t i = 0; i < nPkt; i++)
-    {
-      queue->Enqueue (Create<CobaltQueueDiscTestItem> (Create<Packet> (size), dest, 0, ecnCapable));
-    }
-}
-
-void
-CobaltQueueDiscECNtest::Dequeue (Ptr<CobaltQueueDisc> queue, uint32_t nPkt)
-{
-  for (uint32_t i = 0; i < nPkt; i++)
-    {
-      Ptr<QueueDiscItem> item = queue->Dequeue ();
-    }
-}
-
-void
-CobaltQueueDiscECNtest::DequeueWithDelay (Ptr<CobaltQueueDisc> queue, double delay, uint32_t nPkt)
-{
-  for (uint32_t i = 0; i < nPkt; i++)
-    {
-      Simulator::Schedule (Time (Seconds ((i + 1) * delay)), &CobaltQueueDiscECNtest::Dequeue, this, queue, 1);
-    }
-}
-
-void
-CobaltQueueDiscECNtest::DoRun (void)
-{
-  RunECNtest (QueueSizeUnit::PACKETS);
-  RunECNtest (QueueSizeUnit::BYTES);
-  Simulator::Destroy ();
-}
-
-/**
- * \ingroup traffic-control-test
- * \ingroup tests
- *
- * \brief Test 7: Cobalt Queue Disc Drop Test Item
+ * \brief Test 2: Cobalt Queue Disc Drop Test Item
  */
 class CobaltQueueDiscDropTest : public TestCase
 {
@@ -533,8 +251,6 @@ CobaltQueueDiscDropTest::RunDropTest (QueueSizeUnit mode)
   queue = CreateObject<CobaltQueueDisc> ();
   NS_TEST_EXPECT_MSG_EQ (queue->SetAttributeFailSafe ("MaxSize", QueueSizeValue (QueueSize (mode, modeSize * 100))),
                          true, "Verify that we can actually set the attribute MaxSize");
-  NS_TEST_EXPECT_MSG_EQ (queue->SetAttributeFailSafe ("UseEcn", BooleanValue (true)), true,
-                         "Verify that we can actually set the attribute UseECN");
 
   queue->Initialize ();
 
@@ -595,14 +311,7 @@ public:
     // Test 1: simple enqueue/dequeue with no drops
     AddTestCase (new CobaltQueueDiscBasicEnqueueDequeue (PACKETS), TestCase::QUICK);
     AddTestCase (new CobaltQueueDiscBasicEnqueueDequeue (BYTES), TestCase::QUICK);
-    // Test 2: test NewtonStep() against explicit port of Linux implementation
-    AddTestCase (new CobaltQueueDiscNewtonStepTest (), TestCase::QUICK);
-    // Test 3: test ControlLaw() against explicit port of Linux implementation
-    AddTestCase (new CobaltQueueDiscControlLawTest (), TestCase::QUICK);
-    // Tests 4,5,6: test ECN capability
-    AddTestCase (new CobaltQueueDiscECNtest (), TestCase::QUICK);
-    // Test 7: Drop test
+    // Test 2: Drop test
     AddTestCase (new CobaltQueueDiscDropTest (), TestCase::QUICK);
-
   }
 } g_cobaltQueueTestSuite; ///< the test suite
